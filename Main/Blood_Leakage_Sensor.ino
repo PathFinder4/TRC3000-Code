095c459dfef477f4f06568885b9381b2a8366cf9
Dominic Borcena <dborcena.16@gmail.com>
2021-08-11 11:32:57 +1000

    Adding blood leakage detector

---
+++ Main/Blood_Leakage_Sensor.ino
@@ -0,0 +1,8 @@
+/* Written by [NAME] [STU. NO.]
+ * Last edited [DATE]
+ * INPUT: No input
+ * OUTPUT: Air Detector Readings
+ * PURPOSE: To read air detector and to output information from air detector
+ */
+
+ 
