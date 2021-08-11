c9cfac35889422de85b3a6b4cbc2b7f406287958
Dominic Borcena <dborcena.16@gmail.com>
2021-08-11 11:28:46 +1000

    Adding pressure sensor

---
+++ Main/Pressure_Sensors.ino
@@ -0,0 +1,18 @@
+/* Written by Dominic Borcena 30572460
+ * Last edited 11/08/2021
+ * INPUT: No input
+ * OUTPUT: Pressure sensor readings
+ * PURPOSE: To read the pressure sensor and send as an output when function is called
+ */
+
+void venous_pressure () {
+  // 
+ }
+
+void inflow_pressure () {
+  // 
+ }
+ 
+ void arterial_pressure () {
+  // 
+ }
