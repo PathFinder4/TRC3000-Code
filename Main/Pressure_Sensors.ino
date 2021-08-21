965a152c55963d3630f0b0b13fd516c118ae908c
Dominic Borcena <dborcena.16@gmail.com>
2021-08-21 21:05:39 +1000

    Pressure Sensor Completed

--- Main/Pressure_Sensors.ino
+++ Main/Pressure_Sensors.ino
@@ -5,14 +5,52 @@
  * PURPOSE: To read the pressure sensor and send as an output when function is called
  */
 
-void venous_pressure () {
-  // 
+//initialize variables
+int buzzer = 2;
+float venous_sensor = A0; //will return a voltage
+float arterial_sensor = A1;
+float inflow_sensor = A2;
+float section_area = 2; // assume cross sectional area of force sensor is 2m^2
+
+void setup() 
+{
+  pinMode(venous_sensor,INPUT);
+  pinMode(arterial_sensor,INPUT);
+  pinMode(inflow_sensor,INPUT);
+  pinMode(buzzer,OUTPUT);
+  Serial.begin(9600);
+}
+
+float venous_pressure () {
+  float val = analogRead(venous_sensor); //read sensor value
+  float force_value = map(val, 0, 914, 0, 10); //map force to voltage
+  float pressure_value = forceValue/sectionArea; //gives pressure in kPa
+  
+  return pressure_value;
  }
 
-void inflow_pressure () {
-  // 
+float arterialPressure () {
+  float val = analogRead(arterial_sensor); //read sensor value
+  float forceValue = map(val, 0, 914, 0, 10); //map force to voltage
+  float pressure_value = force_value/section_area; //gives pressure in kPa
+  
+  return pressure_value;
  }
- 
- void arterial_pressure () {
-  // 
- }
+
+float inflowPressure () {
+  float val = analogRead(inflow_sensor); //read sensor value
+  float force_value = map(val, 0, 914, 0, 10); //map force to voltage
+  float pressure_value = force_value/section_area; //gives pressure in kPa
+  
+  return pressure_value;
+}
+
+void loop()
+{
+  float venous = venous_pressure();
+  float arterial = arterial_pressure();
+  float inflow = inflow_pressure();
+  Serial.println(venous);
+  Serial.println(arterial);
+  Serial.println(inflow);
+}
