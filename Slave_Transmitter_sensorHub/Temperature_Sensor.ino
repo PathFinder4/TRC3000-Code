2df9c251153a71e5a8edf847d35a8eda8860ee1c
Michelle <mlee0077@student.monash.edu>
2021-09-01 10:33:59 +1000

    Temperature Sensor Code #2
    Updated Code with Two Functions
    1. Body Temperature Sensor
    2. Dialysate Temperature Sensor

--- Slave_Transmitter_sensorHub/Temperature_Sensor.ino
+++ Slave_Transmitter_sensorHub/Temperature_Sensor.ino
@@ -1,6 +1,6 @@
 /* Written by Michelle Lee 31576443
  * Last edited by Michelle Lee
- * Edited on 22/08/2021
+ * Edited on 01/09/2021
  *
  * Code to program the Temperature Sensor 
  * INPUT: Voltage Reading 
@@ -10,26 +10,49 @@
  * Due to the limitations of TinkerCAD, a TMP36 has been used to replicate TMP117 Temperature Sensor
  *
  */
-
+ 
 #include <Wire.h>
 
-int temp_sensor_input; // Temperature Sensor Input
-int temp_sensor_pin = A0; // Temperature Sensor Pin 
-float temp_celsius; // Temperature in Celsius (Output)
-float temp_sensor_voltage; // Temperature Sensor Input Voltage 
+// Body Temperature Sensor
+int body_temp_sensor_input; // Temperature Sensor Input
+int body_temp_sensor_pin = A0; // Temperature Sensor Pin 
+float body_temp_celsius; // Temperature in Celsius (Output)
+float body_temp_sensor_voltage; // Temperature Sensor Input Voltage 
 
-float temp_sensor()
+// Dialysate Temperature Sensor 
+int dialysate_temp_sensor_input; // Temperature Sensor Input
+int dialysate_temp_sensor_pin = A1; // Temperature Sensor Pin 
+float dialysate_temp_celsius; // Temperature in Celsius (Output)
+float dialysate_temp_sensor_voltage; // Temperature Sensor Input Voltage 
+
+float body_temp_sensor()
 {
   // Receive Input
-  temp_sensor_input = analogRead(temp_sensor_pin);
+  body_temp_sensor_input = analogRead(body_temp_sensor_pin);
   
   // Convert Input to Voltage
-  temp_sensor_voltage = (temp_sensor_input * 5.0)/1024.0;
+  body_temp_sensor_voltage = (body_temp_sensor_input * 5.0)/1024.0;
   
   // Convert Voltage to Temperature in Celsius
   // 10 mV/Degree with 500 mV offset
   // Formula: Temperature = (Voltage - 500 mV) * 100
-  temp_celsius = (temp_sensor_voltage - 0.5) * 100 ;  
+  body_temp_celsius = (body_temp_sensor_voltage - 0.5) * 100 ;  
                                                
-  return temp_celsius;
+  return body_temp_celsius;
+}
+
+float dialysate_temp_sensor()
+{
+  // Receive Input
+  dialysate_temp_sensor_input = analogRead(dialysate_temp_sensor_pin);
+  
+  // Convert Input to Voltage
+  dialysate_temp_sensor_voltage = (dialysate_temp_sensor_input * 5.0)/1024.0;
+  
+  // Convert Voltage to Temperature in Celsius
+  // 10 mV/Degree with 500 mV offset
+  // Formula: Temperature = (Voltage - 500 mV) * 100
+  dialysate_temp_celsius = (dialysate_temp_sensor_voltage - 0.5) * 100 ;  
+                                               
+  return dialysate_temp_celsius;
 }
